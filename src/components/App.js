bfa24a7f20f5fc778f168c3dd0d88253e1a21167
Will W <wjwat@onionslice.org>
2022-04-22 10:38:00 -0500

    Reorganize components, add Header & Footer

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        Epicodus Project Week 16
+      </h1>
+    </header>
+  );
+}
