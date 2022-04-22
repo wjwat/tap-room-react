878caf3f5e8c2e7df782c7fbbf7283b40580fb0b
Will W <wjwat@onionslice.org>
2022-04-22 12:36:29 -0500

    Add BeerControl component

---
+++ src/components/BeerControl.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class BeerControl extends React.Component {
+  constructor(props) {
+    // do stuff
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
