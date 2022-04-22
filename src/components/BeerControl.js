43cc868ff88500c32ecb78da08d6179e10094f0c
Will W <wjwat@onionslice.org>
2022-04-22 16:17:38 -0500

    Add test state to project

--- src/components/BeerControl.js
+++ src/components/BeerControl.js
@@ -1,14 +1,38 @@
 import React from "react";
-import { vs as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
+import BeerList from "./BeerList";
+
+const testBeers = [
+  {
+    id: uuidv4(),
+    name: "Beer 1",
+    brand: "Beer Company 1",
+    price: 10,
+    alcoholContent: 0.1
+  },
+  {
+    id: uuidv4(),
+    name: "Beer 2",
+    brand: "Beer Company 2",
+    price: 5,
+    alcoholContent: 9.9
+  }
+]
 
 export default class BeerControl extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      beers: []
+    };
   }
 
   render() {
     return (
       <>
+        <BeerList beers={testBeers} />
       </>
     )
   }
